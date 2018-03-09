6764d28792e94a5342363699c372bb1847a594a0
Maxime Martin <maxmarti1@publicisgroupe.net>
2018-03-09 18:00:44 +0100

    new architecture app

---
+++ client/src/components/Users.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="posts">This file will list all the users.</div>
+</template>
+
+<script>
+export default {
+  name: 'Users',
+  data () {
+    return {}
+  }
+}
+</script>
---
+++ server/src/app.js
@@ -0,0 +1,23 @@
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var config = require('./config/database');
+
+
+mongoose.connect(config.database);
+
+var api = require('./routes/api');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(passport.initialize());
+
+app.get('/', function(req, res) {
+  res.send('Page under construction.');
+});
+
+app.use('/api', api);
