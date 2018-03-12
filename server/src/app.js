89a15e6e3a13532c3e0deaeab9c4040bf9208b27
Maxime Martin <maxmarti1@publicisgroupe.net>
2018-03-12 18:55:33 +0100

    add features backend

--- server/src/app.js
+++ server/src/app.js
@@ -1,23 +1,23 @@
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 var passport = require('passport');
-var config = require('./config/database');
+var config = require('../config/database');
 
 
 mongoose.connect(config.database);
 
-var api = require('./routes/api');
+var api = require('../routes/api.js');
 
-app.use(function(req, res, next) {
+api.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.use(passport.initialize());
+api.use(passport.initialize());
 
-app.get('/', function(req, res) {
+api.get('/', function(req, res) {
   res.send('Page under construction.');
 });
 
-app.use('/api', api);
+api.use('/api', api);
