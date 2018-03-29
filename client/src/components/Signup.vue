9958979564676e2754cfe9800e7c2f7c6525f975
Martin-Maxime <maximemartin45@msn.com>
2018-03-30 00:30:47 +0200

    add form signup front

--- client/src/components/Signup.vue
+++ client/src/components/Signup.vue
@@ -1,27 +1,50 @@
 <template>
-  <div class="signup">
-    <h1>Inscription</h1>
-    <form @submit.prevent="validateBeforeSubmit" v-if="!formSubmitted">
-      <div class="form-group" :class="{'has-error': errors.has('email') }" >
-        <label class="control-label" for="email">Email</label>
-        <input v-model="email" v-validate="email" data-rules="required|email" class="form-control" type="email" placeholder="Email">
-        <p class="text-danger" v-if="errors.has('email')">{{ errors.first('email') }}</p>
-      </div>
-      <div class="form-group" :class="{'has-error': errors.has('confirm-email') }" >
-        <label class="control-label" for="email">Confirmer l'email</label>
-        <input v-model="confirm-email" v-validate="confirm-email" data-rules="required|confirm-email" class="form-control" type="email" placeholder="Confirmer email">
-        <p class="text-danger" v-if="errors.has('confirm-mail')">{{ errors.first('confirm-email') }}</p>
-      </div>
-      <div class="form-group" :class="{'has-error': errors.has('name') }">
-          <label class="control-label" for="name">Name</label>
-          <input v-model="name" v-validate.initial="name" data-rules="required|alpha|min:3" class="form-control" type="text" placeholder="Full Name">
-          <p class="text-danger" v-if="errors.has('name')">{{ errors.first('name') }}</p>
+  <div class="signup container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Inscription</h1>
       </div>
     </div>
-    <button class="btn btn-primary btn-block" type="submit">Submit</button>
-  </form>
-  <div v-else>
-    <h1 class="submitted">Form submitted successfully!</h1>
+    <div class="row">
+      <form class="col-md-12" @submit.prevent="validateBeforeSubmit" v-if="!formSubmitted">
+          <div class="column is-12">
+              <label class="label">Email</label>
+              <p class="control has-icon has-icon-right">
+                  <input name="email" v-model="email" v-validate.initial="'required|email'" :class="{'input': true, 'is-danger': errors.has('email') }" type="text" placeholder="Email">
+                  <i v-show="errors.has('email')" class="fa fa-warning"></i>
+                  <span v-show="errors.has('email')" class="help is-danger">{{ errors.first('email') }}</span>
+              </p>
+          </div>
+          <div class="column is-12">
+              <label class="label">First Name</label>
+              <p class="control has-icon has-icon-right">
+                  <input name="first_name" v-model="first_name" v-validate.initial="'required|alpha'" :class="{'input': true, 'is-danger': errors.has('first_name') }" type="text" placeholder="First Name">
+                  <i v-show="errors.has('first_name')" class="fa fa-warning"></i>
+                  <span v-show="errors.has('first_name')" class="help is-danger">{{ errors.first('first_name') }}</span>
+              </p>
+          </div>
+          <div class="column is-12">
+              <label class="label">Last Name</label>
+              <p class="control has-icon has-icon-right">
+                  <input name="last_name" v-model="last_name" v-validate.initial="'required|alpha'" :class="{'input': true, 'is-danger': errors.has('last_name') }" type="text" placeholder="Last Name">
+                  <i v-show="errors.has('last_name')" class="fa fa-warning"></i>
+                  <span v-show="errors.has('last_name')" class="help is-danger">{{ errors.first('last_name') }}</span>
+              </p>
+          </div>
+          <div class="column is-12">
+              <label class="label">Full Name</label>
+              <p class="control has-icon has-icon-right">
+                  <input :value="name" name="name" v-validate:name.initial="'required|alpha_spaces'" :class="{'input': true, 'is-danger': errors.has('name') }" type="text" placeholder="Full Name">
+                  <i v-show="errors.has('name')" class="fa fa-warning"></i>
+                  <span v-show="errors.has('name')" class="help is-danger">{{ errors.first('name') }}</span>
+              </p>
+          </div>
+        <button class="btn btn-primary btn-block" type="submit">Submit</button>
+      </form>
+      <div v-else>
+        <h1 class="submitted">Form submitted successfully!</h1>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -39,10 +62,15 @@ VeeValidate.Validator.extend('passphrase', {
 
 export default {
   name: 'Signup',
-  data () {
-    return {
-      msg:'Inscription'
+  data: () => ({
+    email: '',
+    first_name: '',
+    last_name: ''
+  }),
+  computed: {
+    name() {
+      return `${this.first_name} ${this.last_name}`;
     }
   }
-}
+};
 </script>
