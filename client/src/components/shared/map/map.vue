29ca335d5715d8f25a1391ee0b3e35f77c8f7fa0
Maxime Martin <maxmarti1@publicisgroupe.net>
2019-08-13 15:24:03 +0200

    fix vulnerability packages / add geoloc and markers on gmaps

--- client/src/components/shared/map/map.vue
+++ client/src/components/shared/map/map.vue
@@ -3,43 +3,61 @@
 </template>
 
 <script>
+import { serverBus } from "../../../main.js";
+
 export default {
   name: "GoogleMap",
   props: ["name"],
   data: function() {
     return {
+      centerLocation: {
+        lat: 48.864716,
+        lng: 2.349014
+      },
       mapName: this.name + "-map",
-      markerCoordinates: [
-        {
-          latitude: 48.864716,
-          longitude: 2.349014
-        },
-        {
-          latitude: 49.864716,
-          longitude: 1.349014
-        },
-        {
-          latitude: 50.4998973,
-          longitude: 3.349014
-        }
-      ]
+      markerCoordinates: {
+        latitude: 48.864716,
+        longitude: 2.349014
+      }
     };
   },
 
-  mounted: function() {
-    const element = document.getElementById(this.mapName);
-    const options = {
-      zoom: 14,
-      center: new google.maps.LatLng(48.864716, 2.349014)
-    };
-    const map = new google.maps.Map(element, options);
-    this.markerCoordinates.forEach(coord => {
-      const position = new google.maps.LatLng(coord.latitude, coord.longitude);
-      const marker = new google.maps.Marker({
-        position,
-        map
-      });
+  methods: {
+    initMap: function(geolocation) {
+      const element = document.getElementById(this.mapName);
+      const options = {
+        zoom: 14,
+        center: new google.maps.LatLng(geolocation.lat, geolocation.lng)
+      };
+      const map = new google.maps.Map(element, options);
+      if (geolocation) {
+        this.markerCoordinates.latitude = geolocation.lat;
+        this.markerCoordinates.longitude = geolocation.lng;
+
+        const position = new google.maps.LatLng(
+          this.markerCoordinates.latitude,
+          this.markerCoordinates.longitude
+        );
+        const marker = new google.maps.Marker({
+          position,
+          map
+        });
+      }
+    }
+  },
+
+  created() {
+    serverBus.$on("geoLocate", geolocation => {
+      this.geolocation = geolocation;
+      if (this.geolocation) {
+        console.log(this.geolocation);
+        this.initMap(this.geolocation);
+      }
     });
+  },
+
+  mounted: function() {
+    this.initMap(this.centerLocation);
   }
 };
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,17 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import BootstrapVue from 'bootstrap-vue'
-import Vue from 'vue'
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Vue from 'vue';
 //import App from './App'
-import App from './App'
-import router from './router'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
+import App from './App';
 // Import App sass
-import './assets/style/app.scss'
+import './assets/style/app.scss';
+import router from './router';
 
-Vue.config.productionTip = false
+export const serverBus = new Vue();
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
@@ -19,4 +19,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
